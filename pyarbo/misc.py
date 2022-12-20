e084ad5065c7e28bc63c5abb6e26f23dd9755275
alexdesiqueira <alex.desiqueira@igdore.org>
2022-12-20 18:20:48 -0300

    New version of misc.py

--- pyarbo/misc.py
+++ pyarbo/misc.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def fill_missing_dates(data, format="%Y-%m-%d"):
-    """Fills missing dates from a pandas dataframe. 
+    """Fills missing dates from a pandas dataframe.
 
     Parameters
     ----------
@@ -12,7 +12,7 @@ def fill_missing_dates(data, format="%Y-%m-%d"):
     Returns
     -------
     data : pandas.DataFrame
-        Data reindexed with filled missing data. 
+        Data reindexed with filled missing data.
 
     Notes
     -----
@@ -23,17 +23,15 @@ def fill_missing_dates(data, format="%Y-%m-%d"):
     except ParserError:
         raise
 
-    date_min_max = pd.date_range(
-        data.index.min(),
-        data.index.max()
-    )
+    date_min_max = pd.date_range(data.index.min(), data.index.max())
     data = data.reindex(date_min_max, fill_value=0)
 
     return data
 
 
 def return_dengue_cases(data):
-    """Return dengue cases, classified in notified, probable, and lab confirmed.
+    """Return dengue cases, classified in notified, probable, and lab
+       confirmed.
 
     Parameters
     ----------
@@ -47,14 +45,14 @@ def return_dengue_cases(data):
     -----
     Expected columns: "dt_sin_pri", "classi_fin", "criterio"
     """
-    data = data.assign(tipo = "notified")
+    data = data.assign(tipo="notified")
 
-    classifin_not_5 = (data["classi_fin"] != 5)
-    criterio_is_1 = (data["criterio"] == 1)
+    class_not_5 = data["classi_fin"] != 5
+    criterio_is_1 = data["criterio"] == 1
 
-    data.loc[:, (classifin_not_5, "tipo")] = "probable"
+    data.loc[:, (class_not_5, "tipo")] = "probable"
 
-    dengue_mask = classifin_not_5 & criterio_is_1
+    dengue_mask = class_not_5 & criterio_is_1
     data[dengue_mask]["tipo"] = "lab_confirmed"
 
     return data
